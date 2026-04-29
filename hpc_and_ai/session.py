2997a1dcad3671530f75116e2a72fd62a04405c5
Vera Linno <v.veralinno@gmail.com>
2026-04-29 15:01:43 +0300

    Create session.py

---
+++ hpc_and_ai/session.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+
+DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
